40ababb1ac210579bffd3284c21e0963bbd0d59e
Jorge Antonio <jorgeantonio.acll@gmail.com>
2022-03-11 16:49:45 -0300

    implementaçao do calculo

--- s.js
+++ s.js
@@ -1,1 +1,18 @@
-alert("oi")
+function verificar() {
+    var data = new Date
+    var ano = data.getFullYear()
+    var fano = document.getElementById("ano")
+    var res = document.getElementById("res")
+
+    if (fano.value.length == 0 || Number(fano.value) > ano){
+        alert("[ERRO] Verifique os dados e tente novamente")
+    }else{
+        var radt = document.getElementsByName("radt")
+        var idade = ano  - Number(fano.value)
+        
+    }
+
+
+
+
+}
