a7258cfd4ccb53023422d6b985ba4d1eb69f11f2
Jorge Antonio <jorgeantonio.acll@gmail.com>
2022-03-11 17:17:57 -0300

    ProjFinalizado

--- s.js
+++ s.js
@@ -7,12 +7,39 @@ function verificar() {
     if (fano.value.length == 0 || Number(fano.value) > ano){
         alert("[ERRO] Verifique os dados e tente novamente")
     }else{
-        var radt = document.getElementsByName("radt")
+        var fsex = document.getElementsByName("radt")
         var idade = ano  - Number(fano.value)
-        
+        var genero = ""
+        var img = document.createElement("img")
+        img.setAttribute("id","foto")
+
+        if(fsex[0].checked){
+            genero = "Homem"
+            if( idade>=0 && idade<10){
+                img.setAttribute("src","bebeh.png")
+            }else if( idade >= 10 && idade<21){
+                img.setAttribute("src","jovenh.png")
+            }else if ( idade >= 21 && idade < 50){
+                img.setAttribute("src","homemA.png")
+            }else{
+                img.setAttribute("src","homemI.png")
+            }
+        }else if(fsex[1].checked){
+            genero = "Mulher"
+
+            if( idade>=0 && idade<10){
+                img.setAttribute("src","bebem.png")
+            }else if( idade >= 10 && idade<21){
+                img.setAttribute("src","jovenm.png")
+            }else if ( idade >= 21 && idade < 50){
+                img.setAttribute("src","mulherA.png")
+            }else{
+                img.setAttribute("src","mulheri.png")             
+            }
+        }
+        res.style.textAlign= "center"
+        res.innerHTML= `Detectado ${genero} com ${idade} anos` 
+        res.appendChild(img)
     }
-
-
-
 
 }
